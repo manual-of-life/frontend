b8fafd3441a0595fe37ed4fedc6794a1602cd546
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-18 19:26:57 +0200

    first attempt at a first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="o-crop">
+      <article className="o-crop__content o-crop__content--center o-media">
+        <section className="o-media__img o-media__img--reverse">
+          <img alt="stamp - paid in full by his blood" src="./assets/luke/chapter1/crownofthorns_stamp.png"/>
+        </section>
+        <section className="o-media__body">
+          <h1 className="u-font-family__decorative--handy--beauty u-align--center u-color--neutral-secondary">Ehrenwertester Freund Gottes!</h1>
+          <ol>
+            <li className="o-bullet--block u-radius--medium u-shadow--elevated-bottom">
+              <span className="o-bullet o-bullet__icon">1.</span>
+              <p className="o-bullet o-bullet__text">
+                Weil [wie es wohlbekannt ist] Viele das Unternehmen in Angriff genommen haben eine Erz&auml;hlung (einen roten Faden durch die Geschichten) in Reihenfolge zusammenzustellen und zu organisieren: von den gesicherten und etablierten Taten, die in und unter uns zu Wege gebracht und erf&uuml;llt wurden-
+              </p>
+            </li>
+            <li className="o-bullet--block u-radius--medium u-shadow--elevated-bottom">
+              <span className="o-bullet o-bullet__icon">2.</span>
+              <p className="o-bullet o-bullet__text">
+                genau so wie sie an uns weitergegeben wurden durch Jene, die vom [offiziellen] Anfang [vom Dienst von Yeshua] an Augenzeugen und Minister des Wortes [genauer gesagt, der Lehre (Doktrin) bez&uuml;glich des Erlangens der Erl&ouml;sung durch Christus im K&ouml;nigreich Gottes] waren -
+              </p>
+            </li>
+            <li className="o-bullet--block u-radius--medium u-shadow--elevated-bottom">
+              <span className="o-bullet o-bullet__icon">3.</span>
+              <p className="o-bullet o-bullet__text">
+                erschien es mir gut und erstrebenswert, [und deswegen beschloss ich] schlie&szlig;lich, auch nachdem ich gr&uuml;ndlich alle Dinge herausgesucht habe und allen Dingen dringlich gefolgt bin und ihren Verlauf akkurat nachverfolgt habe - von den allerh&ouml;chsten Dingen bis zum minuti&ouml;sesten Detail - vom Anfang an, f&uuml;r Dich einen geordneten Bericht zu schreiben, ehrenwertester Freund Gottes [Theophilus (griech); dt.: Gottlieb, Amadeus, Freund Gottes],
+              </p>
+            </li>
+            <li className="o-bullet--block u-radius--medium u-shadow--elevated-bottom">
+              <span className="o-bullet o-bullet__icon">4.</span>
+              <p className="o-bullet o-bullet__text">
+                [meine Absicht dabei:] damit Du die ganze Wahrheit kennst und mit Gewissheit und Sicherheit vor Fehlern wei&szlig;t und die (historischen) Berichte und Lehren (Doktrinen) dieses Glaubens, dessen Du informiert worden bist und von dem Du m&uuml;ndlich Anweisungen empfangen hast, verstehst.
+              </p>
+            </li>
+          </ol>
+          <h3 className="u-align--right u-font-family__accent u-color--neutral-secondary">
+            <cite>
+              Lukas 1:1-4
+            </cite>
+          </h3>
+        </section>
+      </article>
+    </section>
   );
 }
 
